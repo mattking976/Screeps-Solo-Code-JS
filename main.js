741d45b91ce49124e1446e811f2546e75e7c0539
Matthew King1 <matthew.king1@fdmgroup.com>
2022-07-16 00:41:13 +0100

    Setting up the harvester file
    setup the harvester file and removed the harvester code from main.js

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
+//main module should be very little here that actually controls anything
 module.exports.loop = function () {
 	// Your code goes here
-	for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-
-        if(creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
-            }
-        }
-    }
+	
 }
