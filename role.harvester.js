90a2d67c80f0e3899759cfb49dc5922aed0a8d1f
Matthew King1 <matthew.king1@fdmgroup.com>
2022-07-16 21:09:18 +0100

    HOTFIX
    Fixed upgrader and builder code as they failed to run

--- role.harvester.js
+++ role.harvester.js
@@ -2,26 +2,26 @@ const { filter } = require("lodash");
 
 //harvester ai code
 var roleHarvester = {
-    /** @param {Creep} creep **/
 
-    run: function(creep){
-        if(creep.store.getFreeCapacity() > 0){
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE){
-                creep.moveTo(sources);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
         }
-        else{
-            //targeting code for stores other than the spawner.
+        else {
             var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (Structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || 
-                        structure.structureType == STRUCTURE_SPAWN) && 
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;}
-                });
-            if(targets.length > 0){
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-                {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_TOWER) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
--- role.upgrader.js
+++ role.upgrader.js
@@ -1,23 +1,25 @@
 var roleUpgrader = {
+
     /** @param {Creep} creep **/
+    run: function(creep) {
 
-    run: function(creep){
-
-        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0){
+        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
         }
-        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0){
-            creep.upgrading = true;
+        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+            creep.memory.upgrading = true;
+            creep.say('⚡ upgrade');
         }
 
-        if(creep.memory.upgrading){
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE){
+        if(creep.memory.upgrading) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
         }
-        else{
+        else {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE){
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
