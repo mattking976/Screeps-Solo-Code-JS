76f64da06214942af6dc622b2b6561fad1ede50e
Matthew King1 <matthew.king1@fdmgroup.com>
2022-07-16 17:19:47 +0100

    begun updates for builders
    updated main and created the builder role.

--- role.builder.js
+++ role.builder.js
@@ -1,1 +1,30 @@
 //builder code for creating structures.
+var roleBuilder = {
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.memory.building && creep.store[RESOURCE_ENERGY]){
+            creep.memory.building = false;
+            creep.say('harvest');
+        }
+        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+            creep.memory.building = true;
+            creep.say('building');
+        }
+        if(creep.memory.building == true) {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(targets[0]);
+                }
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0] == ERR_NOT_IN_RANGE)){
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}})
+            }
+        }
+    }
+};
+
+module.exports = roleBuilder;
