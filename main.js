81881ff81071b64dc29a6e54375854ddc667c498
Matthew King1 <matthew.king1@fdmgroup.com>
2022-07-16 10:41:04 +0100

    added roles
    Added the harvester and upgrader roles to the code. NOTE: no auto spawning as of yet

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 //main module should be very little here that actually controls anything
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.harvester');
+
 module.exports.loop = function () {
 	// Your code goes here
-	
+
+	//checking creep role
+	for(var name in Game.creeps){
+		var creep = Game.creeps[name];
+		if(creep.memory.role == 'harvester'){
+			roleHarvester.run(creep);
+		}
+		if(creep.memory.role == 'upgrader'){
+			roleUpgrader.run(creep);
+		}
+	}
 }
